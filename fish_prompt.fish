dc720f13cc09c376d8a348072aa39590adcd2f43
Gustav Sörnäs <gustav@sornas.net>
2021-10-03 18:50:18 +0200

    git-repo path blue and no lead

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,12 +1,20 @@
 function fish_prompt
-    set -l user
     if test -n "$SSH_CONNECTION"
         echo -n "$USER::$hostname<"
     else
         echo -n "::<"
     end
-    set_color green
-    echo -n (string replace -r '^'"$HOME" '~' "$PWD")
+
+    set -l git_root (command git rev-parse --show-toplevel 2>/dev/null)
+    if test -n "$git_root"
+        set_color blue
+        set -l git_bn (command basename "$git_root")
+        echo -n (string replace -r '^'"$git_root" "$git_bn" "$PWD")
+    else
+        set_color green
+        echo -n (string replace -r '^'"$HOME" '~' "$PWD")
+    end
     set_color normal
+
     echo -n "> "
 end
